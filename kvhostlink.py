1470f3e68959acf4a3ed2d9950b3afb4f605600d
OkitaSystemDesign <97392082+OkitaSystemDesign@users.noreply.github.com>
2026-04-16 20:44:55 +0900

    416

--- kvhostlink.py
+++ kvhostlink.py
@@ -95,34 +95,35 @@ class kvHostLink:
         return rcv
 
 if __name__ == "__main__":
-    kv = kvHostLink('192.168.0.31')
+    kv = kvHostLink('192.168.250.32')
     data = kv.mode('1')
-    print(data)
-    data = kv.er()
-    print(data)
-    data = kv.errclr()
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.unittype()
-    print(data)
+    print(data)						# b'57\r\n'
+    data = kv.errclr()
+    print(data)						# b'OK\r\n'
+    data = kv.er()
+    print(data)						# b'000\r\n'
     data = kv.settime()
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.set('MR0')
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.reset('MR1')
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.sts('MR10', 5)
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.rss('MR10', 4)
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.read('DM0.U')
-    print(data)
+    print(data)						# b'00002\r\n'
     data = kv.reads('DM0.S', 4)
-    print(data)
+    print(data)						# b'+00002 +00001 +00002 +00003\r\n'
     data = kv.write('DM0.U', '2')
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.writs('DM1.S', 4, '1 2 3 4')
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.mws('DM0.H DM1.S DM2.L DM4.U DM5.D')
-    print(data)
+    print(data)						# b'OK\r\n'
     data = kv.mwr()
-    print(data)
+    print(data)						# b'0002 +00001 +0000196610 00004 0605955594\r\n'
+
